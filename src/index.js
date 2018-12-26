decf8f00de8a1fcb6cfa1f17854ff2ba116ab110
Spencer Arnold <sa.variable@gmail.com>
2018-12-26 16:25:05 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App'; //importing our componenets here.
+import * as serviceWorker from './serviceWorker'; //Allows for js to be executed in the background.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//This file is the entry point for the react app.
+//As you can see, we render whatever components we have built here.
+
+ReactDOM.render(<App />, document.getElementById('root')); //render app under root.
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
